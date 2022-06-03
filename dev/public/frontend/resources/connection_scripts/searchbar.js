9641fea19f44b47a0272bdf0b6c86b399fb04cff
deliagrigorita <deliagrigorita@yahoo.com>
2022-06-03 21:40:09 +0300

    search bar

--- dev/public/frontend/resources/connection_scripts/searchbar.js
+++ dev/public/frontend/resources/connection_scripts/searchbar.js
@@ -1,8 +1,24 @@
   const url = '/courses/search';
 
-fetch(url)
-  .then(response => response.json())
-  .then(repos => {
-    console.log(repos);
-  })
-.catch(err => console.log(err))
+  const handleSubmit = (formElement) => {
+    formElement.preventDefault();
+    let data = {};
+
+    [...formElement.currentTarget.elements]
+        .filter((inputElement) => inputElement.type !== "submit")
+        .forEach((inputElement) => {
+            data[inputElement.getAttribute("name")] = inputElement.type === "file" ? inputElement.files : inputElement.value
+        });
+
+        fetch(url)
+        .then(response => response.json())
+        .then(repos => {
+          console.log(repos);
+        })
+      .catch(err => console.log(err))
+
+    return data
+}
+
+const searchCoursesForm = document.getElementById("searchCoursesForm")
+ searchCoursesForm.addEventListener("submit", handleSubmit);
